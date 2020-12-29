9dce9befe0460321cc69e02da47b49b94b092efc
copes <copes.km@gmail.com>
2020-12-29 23:18:23 +0900

    다이나믹 프로그래밍 - 1로 만들기
    1. 초기값 설정 (명확하게)
    2. 점화식
    
    - 리스트는 처음에 칸을 만들어줄때 주의해줘야 한다.

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/1463 1로 만들기 - 초기값 설정 명확하게.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+
+dp = [0 for _ in range(n+1)]
+
+# 1. 초기값 설정
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+# 2. 점화식
+for i in range(4, n+1):
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+
+print(dp[n])
+    
+        
+
+
+
+
