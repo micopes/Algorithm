6e45617ac2711ee7ba14f4b1dbccf1eadf9d4016
copes <copes.km@gmail.com>
2021-01-04 12:59:24 +0900

    DP
    - dp에 어떤 값을 저장할 것인지(어떤 것을 소문제로 볼 것인지.)

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/DP/11054 가장 긴 바이토닉 부분 수열 - 값을 어떻게 저장하며 나아갈 것인지, 무엇을 출력할 것인지 작은 문제로 생각!.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+a = list(map(int, input().split()))
+
+dp1 = [1 for _ in range(1000)] 
+dp2 = [1 for _ in range(1000)]
+
+for i in range(n):
+    for j in range(i+1, n):
+        if a[i] < a[j] and dp1[j] < dp1[i] + 1:
+            dp1[j] = dp1[i] + 1
+
+for i in range(n-1, -1, -1):
+    for j in range(i-1, -1, -1):
+        if a[i] < a[j] and dp2[j] < dp2[i] + 1:
+            dp2[j] = dp2[i] + 1
+
+result = 0
+for i in range(n):
+    result = max(result, dp1[i] + dp2[i])
+
+print(result-1)
