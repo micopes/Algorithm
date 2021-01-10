d6a79338083f508a934d83ec99e481b3cf61b9d4
copes <copes.km@gmail.com>
2021-01-10 13:56:52 +0900

    큐(덱)
    - 선형 큐를 리스트로 직접 구현하면 맨앞의 요소를 뽑아낼 때 O(n)의 시간복잡도가 요구된다.
    - import queue를 통해서 구현하면 front를 하는데 O(1)의 시간복잡도로 구현하는 방법이 마땅치 않다.
    - 따라서 O(1)의 시간복잡도로 해결하기 위해 from collections import deque을 통해 모듈에서 deque를 가져와서 활용하였다.

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/Stack, Queue, Dequeue, PriorityQueue/18258 큐 2.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+d = deque()
+
+n = int(input())
+
+for i in range(n):
+	string = input().split()
+	if string[0] == 'push':
+		d.append(string[1])
+	elif string[0] == 'pop':
+		if len(d) == 0:
+			print(-1)
+		else:
+			print(d.popleft())
+	elif string[0] == 'size':
+		print(len(d))
+	elif string[0] == 'empty':
+		if len(d) == 0:
+			print(1)
+		else:
+			print(0)
+	elif string[0] == 'front':
+		if len(d) == 0:
+			print(-1)
+		else:
+			print(d[0])
+	elif string[0] == 'back':
+		if len(d) == 0:
+			print(-1)
+		else:
+			print(d[-1])
+
