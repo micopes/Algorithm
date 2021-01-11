160915c9f7266aa060755fe7cc306fdaa3ab9b93
copes <copes.km@gmail.com>
2021-01-11 12:34:09 +0900

    소수
    - 소수는 자신 이외의 약수가 1뿐인 수

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/기타/1929 소수 구하기 - 소수는 자신 이외의 약수가 1뿐인 수.py
@@ -0,0 +1,22 @@
+import math
+
+sosu = [1 for _ in range(1000001)]
+sosu[1] = 0
+sosu[2] = 1
+sosu[3] = 1
+
+M, N = map(int, input().split())
+
+for i in range(4, N+1):
+    for j in range(2, int(math.sqrt(i))+1):
+        if i % j == 0:
+            sosu[i] = 0
+            break
+        if j*j > i:
+            break
+
+for i in range(M, N+1):
+    if sosu[i] == 1 :
+        print(i)
+
+        
