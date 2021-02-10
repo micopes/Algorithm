7a7b8358eaefc838c15f8b343e4bc96053945d41
copes <copes.km@gmail.com>
2021-02-10 20:59:26 +0900

    구현

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/기타/14581.py
@@ -0,0 +1,11 @@
+import sys
+# input = sys.stdin.readline
+
+hongs_id = input().rstrip()
+pan_imo = ":fan:"
+
+print("%s%s%s" %(pan_imo, pan_imo, pan_imo))
+print("%s:%s:%s" %(pan_imo, hongs_id, pan_imo))
+print("%s%s%s" %(pan_imo, pan_imo, pan_imo))
+     
+    
