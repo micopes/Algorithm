1abb0a0775501ad9f01d20352c5739d0a1c46e12
copes <copes.km@gmail.com>
2021-02-14 20:12:57 +0900

    DP, 슬라이딩 윈도우
    메모리 제한.
    DP에 모든 것을 저장할 필요가 없다.

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/DP/2096 내려가기 - 메모리 제한(DP, 슬라이딩 윈도우).py
@@ -0,0 +1,26 @@
+import sys
+# input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+box = []
+for i in range(n):
+    box.append(list(map(int, input().rstrip().split())))
+   
+dp1 = [0, 0, 0]
+dp2 = [0, 0, 0]
+
+dp1[0] = dp2[0] = box[0][0]
+dp1[1] = dp2[1] = bo9x[0][1]
+dp1[2] = dp2[2] = box[0][2]
+
+for i in range(1, n):
+    dp1[0], dp1[1], dp1[2] = max(dp1[0], dp1[1]) + box[i][0],\
+        max(dp1[0], dp1[1], dp1[2]) + box[i][1],\
+        max(dp1[1], dp1[2]) + box[i][2]
+    
+    dp2[0], dp2[1], dp2[2] = min(dp2[0], dp2[1]) + box[i][0],\
+        min(dp2[0], dp2[1], dp2[2]) + box[i][1],\
+        min(dp2[1], dp2[2]) + box[i][2]
+
+print(max(dp1), min(dp2))
