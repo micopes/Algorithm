628fb047df7957f1ae9dc9d9f01ada8e0a831405
copes <copes.km@gmail.com>
2021-02-16 22:39:07 +0900

    완전 탐색(백트래킹)

--- 알고리즘 - 파이썬/백준(acmicpc.net)/완전 탐색/1987 알파벳.py
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/완전 탐색/1987 알파벳.py
@@ -1,38 +1,32 @@
 import sys
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def backtrack(x, y, visitedAlpha, cnt):
     global ans
-    if cnt > ans:
-        ans = cnt
+    ans = max(ans, cnt)
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < R and 0 <= ny < C:
-            if visited[nx][ny] == 0 and board[nx][ny] not in visitedAlpha:
-                visited[nx][ny] = 1
-                visitedAlpha.append(board[nx][ny])
+        if 0 <= nx < R and 0 <= ny < C and board[nx][ny] not in visitedAlpha:
+                visitedAlpha += board[nx][ny]
                 backtrack(nx, ny, visitedAlpha, cnt+1)
-                visited[nx][ny] = 0
-                visitedAlpha.pop()
+                visitedAlpha = visitedAlpha[:-1]
                 
-
 # [행][열]
 R, C = map(int, input().rstrip().split())
 board = [] # [0~R-1][0~C-1]
 for i in range(R):
     string = input().rstrip()
     board.append(list(string))
-    
-visited = [[0]*C for _ in range(R)] # [R-1][C-1]
 
 # 좌측 상단 처리
-visited[0][0] = 1
-visitedAlpha = [board[0][0]]
+visitedAlpha = ""
+visitedAlpha += board[0][0]
 ans = 1
+
 backtrack(0, 0, visitedAlpha, 1)
 
 print(ans)
