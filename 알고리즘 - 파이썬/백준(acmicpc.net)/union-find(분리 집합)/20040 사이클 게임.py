f46c91fe35e9b9ca34c1e1d3fc37ba786fc55dd5
copes <copes.km@gmail.com>
2021-10-05 20:23:47 +0900

    Union-Find (Cycle)

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/union-find(분리 집합)/20040 사이클 게임.py
@@ -0,0 +1,38 @@
+import sys
+# input = sys.stdin.readline
+
+def find(n):
+    if n == parent[n]:
+        return n
+
+    parent[n] = find(parent[n])
+    return parent[n]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    
+    if a == b:
+        return 0
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+    
+    return 1
+
+n, m = map(int, input().rstrip().split())
+parent = [i for i in range(n)]
+
+ans = 0
+for i in range(m):
+    a, b = map(int, input().rstrip().split())
+    
+    # 원래 부모가 같았던 경우
+    if union(a, b) == 0:
+        ans = i+1
+        break
+
+print(ans)
+        
