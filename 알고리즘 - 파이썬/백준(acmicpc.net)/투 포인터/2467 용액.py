de1cf535278b69b9bdd98bf82475c781f049a259
copes <copes.km@gmail.com>
2021-10-05 17:23:07 +0900

    투 포인터

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/투 포인터/2467 용액.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+arr = list(map(int, input().rstrip().split()))
+
+left = 0
+right = n-1
+answer_left = -1
+answer_right = -1
+min_val = sys.maxsize
+
+while left < right:
+    sum_val = arr[left] + arr[right]
+    
+    if min_val > abs(sum_val):
+        min_val = abs(sum_val)
+        answer_left = left
+        answer_right = right
+    
+    if sum_val > 0:
+        right -= 1
+    elif sum_val < 0:
+        left += 1
+    else:
+        break
+
+print(arr[answer_left], arr[answer_right])
+    
