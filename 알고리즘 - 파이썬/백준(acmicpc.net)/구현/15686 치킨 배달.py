2ee878aab2ae51585d1587cbd92dad84e5e6ec99
copes <copes.km@gmail.com>
2021-10-07 20:08:02 +0900

    구현

---
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/구현/15686 치킨 배달.py
@@ -0,0 +1,50 @@
+from itertools import combinations
+import sys
+
+# input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+
+city = []
+
+for i in range(n):
+    city.append(list(map(int, input().rstrip().split())))
+    
+
+home = []
+chicken = []
+
+for i in range(n):
+    for j in range(n):
+        if city[i][j] == 1:
+            home.append([i, j])
+        elif city[i][j] == 2:
+            chicken.append([i, j])
+ans = sys.maxsize
+
+for k in combinations(chicken, m):
+    chicken = []
+    temp_ans = 0
+    for ch in k:
+        chicken.append(ch)
+    
+    for i in range(len(home)):
+        home_x = home[i][0]
+        home_y = home[i][1]
+        dist = sys.maxsize
+        for j in range(len(chicken)):
+            chicken_x = chicken[j][0]
+            chicken_y = chicken[j][1]
+            temp = abs(home_x - chicken_x) + abs(home_y - chicken_y)
+            if dist > temp:
+                dist = temp
+        # print(dist)
+        temp_ans += dist
+        
+    if temp_ans < ans:
+        ans = temp_ans
+
+print(ans)
+        
+    
+    
