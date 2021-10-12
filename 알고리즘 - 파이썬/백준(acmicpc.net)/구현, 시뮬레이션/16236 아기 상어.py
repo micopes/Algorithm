11e495907dd61b646a1e11f4a2df61f389fd1a9d
copes <copes.km@gmail.com>
2021-10-12 18:09:18 +0900

    BFS, 시뮬레이션, 구현

--- 알고리즘 - 파이썬/백준(acmicpc.net)/구현, 시뮬레이션/16236 아기 상어.py
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/구현, 시뮬레이션/16236 아기 상어.py
@@ -69,4 +69,75 @@ while True:
 
 print(stage)
     
+# %%
+# 방법 2
+
+import sys
+from collections import deque
+# input = sys.stdin.readline
+
+n = int(input().rstrip())
+graph = []
+
+for i in range(n):
+    temp = list(map(int, input().rstrip().split()))
+    graph.append(temp)
+
+r, c = -1, -1
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 9:
+            graph[i][j] = 0
+            r, c = i, j
+            break
+        
+size = 2
+eat = 0
+stage = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+while True:
+    q = deque()
+    q.append([r, c, 0])
+    
+    fish = []
+    visited = [[0 for j in range(n)] for i in range(n)]
+    visited[r][c] = 1
+    
+    while q:
+        x, y, count = q.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0:
+                if 0 < graph[nx][ny] < size:
+                    q.append([nx, ny, count+1])
+                    fish.append([nx, ny, count+1])
+                    visited[nx][ny] = 1
+                    flag = count
+                
+                if graph[nx][ny] == 0 or graph[nx][ny] == size:
+                    q.append([nx, ny, count+1])
+                    visited[nx][ny] = 1
+        
+    
+    if len(fish) > 0:
+        fish.sort(key = lambda x : (x[2], x[0], x[1]))
+        r, c, count = fish[0][0], fish[0][1], fish[0][2]
+        stage += count
+        graph[r][c] = 0
+        eat += 1
+        if eat == size:
+            size += 1
+            eat = 0
+    else:
+        break
+
+print(stage)
+    
+
 
