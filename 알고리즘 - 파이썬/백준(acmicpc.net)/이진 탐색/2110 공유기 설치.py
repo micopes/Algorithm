9e15875f9a59fe2627993ca43bd2b48fc760c9c2
copes <copes.km@gmail.com>
2021-11-10 17:33:56 +0900

    이분 탐색 - 거리를 start, mid, end로

--- 알고리즘 - 파이썬/백준(acmicpc.net)/이진 탐색/2110 공유기 설치.py
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/이진 탐색/2110 공유기 설치.py
@@ -16,7 +16,7 @@ ans = 0
 
 while start <= end:
     mid = (start + end) // 2
-    cnt = 1
+    cnt = 1 # 맨 앞에서 맨 뒤까지 1개 추가하고 시작해야 한다. 나누는 것이므로 개수 +1
     old = house[0]
     
     # house[i]와 house[old]간의 거리가 mid 이상인 것을 하나씩 찾으면서 진행한다.
