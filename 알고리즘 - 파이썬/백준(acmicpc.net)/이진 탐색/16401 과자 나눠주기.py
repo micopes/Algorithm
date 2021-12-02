c3939860dba25cf46c75428fd2fbd616145f2f63
copes <copes.km@gmail.com>
2021-12-03 00:07:03 +0900

    이분 탐섹

--- 알고리즘 - 파이썬/백준(acmicpc.net)/이진 탐색/16401 과자 나눠주기.py
+++ 알고리즘 - 파이썬/백준(acmicpc.net)/이진 탐색/16401 과자 나눠주기.py
@@ -1,31 +1,28 @@
 import sys
+# input = sys.stdin.readline
 
-# 조카의 수, 과자의 수
-m, n = map(int, sys.stdin.readline().split())
-# 과자의 길이
-arr = list(map(int, sys.stdin.readline().split()))
+m, n = map(int, input().rstrip().split())
+
+arr = list(map(int, input().rstrip().split()))
 
 start = 0
 end = max(arr)
 
-result = 0
-while (start <= end):
-    total = 0
+ans = 0
+while start <= end:
+    poss_num = 0
     mid = (start + end) // 2
-    # 모든 조카에게 같은 길이의 막대과자를 나눠줄 수 없을 때
     if mid == 0:
-        total = 0
         break
     
     for x in arr:
         if x >= mid:
-            total += (x//mid)
-
-    if total >= m:
+            poss_num += x//mid
+    
+    if poss_num >= m:
         start = mid + 1
-        result = mid
-        
+        ans = mid
     else:
         end = mid - 1
-
-print(result)
+    
+print(ans)
