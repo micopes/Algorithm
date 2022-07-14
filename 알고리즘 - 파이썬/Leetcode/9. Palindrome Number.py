050ae724745224c0c55c6d42af9da0b0a4382560
Minjin Kang <43158502+micopes@users.noreply.github.com>
2022-07-14 20:53:51 +0900

    Delete 9. Palindrome Number.py

--- 알고리즘 - 파이썬/Leetcode/9. Palindrome Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        
-        string = str(x)
-        rev_string = string[::-1]
-        if string == rev_string:
-            return True
