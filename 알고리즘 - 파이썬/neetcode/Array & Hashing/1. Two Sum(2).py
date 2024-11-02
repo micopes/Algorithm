d04ac2fe2852d58069531d889cc34f0c55d6ef38
Minjin Kang <43158502+micopes@users.noreply.github.com>
2024-11-02 09:56:46 +0900

    딕셔너리 탐색 (O(1)) 을 이용한 시간복잡도 O(N)

--- 알고리즘 - 파이썬/neetcode/Array & Hashing/1. Two Sum(2).py
+++ 알고리즘 - 파이썬/neetcode/Array & Hashing/1. Two Sum(2).py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 딕셔너리 이용 탐색(O(1))을 이용한 O(N) 시간복잡도
         num_dic = dict() # key, value == val, idx
         for i in range(len(nums)):
             num_dic[nums[i]] = i # 동일한 키인 경우에는 이후에 저장된 것만 남음.
