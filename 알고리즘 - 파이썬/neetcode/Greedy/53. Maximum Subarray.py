8c4d55f03d6e5fe0318aca9c8bf1a5928594fdad
Minjin Kang <43158502+micopes@users.noreply.github.com>
2024-11-07 22:09:32 +0900

    Kadane’s Algorithm

--- 알고리즘 - 파이썬/neetcode/Greedy/53. Maximum Subarray.py
+++ 알고리즘 - 파이썬/neetcode/Greedy/53. Maximum Subarray.py
@@ -1,7 +1,5 @@
-import copy
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int: 
-        # 1. 누적합 응용
         for i in range(1, len(nums)):
             if nums[i-1] > 0:
                 nums[i] += nums[i-1]
